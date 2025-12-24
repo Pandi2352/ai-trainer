2dced0249e8bcbccbb0f62761acbb4483b2c699b
Pandi2352 <mvp.bosepandi@gmail.com>
2025-12-24 10:14:52 +0530

    feat: add DashboardLayout component with authentication, sidebar, and navbar

--- client/src/components/layout/DashboardLayout.tsx
+++ client/src/components/layout/DashboardLayout.tsx
@@ -21,8 +21,8 @@ const DashboardLayout: React.FC = () => {
             <Sidebar />
             <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
                 <Navbar />
-                <main className="flex-1 overflow-auto p-6 scroll-smooth">
-                    <div className="max-w-7xl mx-auto w-full">
+                <main className="flex-1 overflow-auto scroll-smooth">
+                    <div className=" mx-auto w-full">
                         <Outlet />
                     </div>
                 </main>
